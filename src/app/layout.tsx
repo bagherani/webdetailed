57be5f110a3b08a534c32a1f25785da191f30c47
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-11-07 17:22:31 +0100

    feat: google analytics added to the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/header/header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,8 @@ export default function RootLayout({
       >
         <Header />
         {children}
+
+        <GoogleAnalytics gaId="G-8DJTL06RKD" />
       </body>
     </html>
   );
