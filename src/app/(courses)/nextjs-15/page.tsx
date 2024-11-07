d687f68ab0aa3f03080c9f9a2d0751d9af61ba8f
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-11-07 17:33:59 +0100

    feat: some links to pages created

--- src/app/(courses)/nextjs-15/page.tsx
+++ src/app/(courses)/nextjs-15/page.tsx
@@ -1,3 +1,36 @@
+import Link from "next/link";
+
 export default function NextJS() {
-  return <div>1</div>;
+  return (
+    <div>
+      <h1>Next.js 15 full course</h1>
+      <ol>
+        <li>
+          <Link href="/nextjs-15/intro">Intro to Next.js</Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/create-next-app">Create Next.js App</Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/project-structure">Project Structure</Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/configure-debugging">Configure Debugging</Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/routing-in-nextjs">
+            Basic Routing in Next.js
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/layouts-in-nextjs">Layouts in Next.js</Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/project-organization">
+            Project Organization
+          </Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,24 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="bg-blue-600 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">My Website</h1>
+        <h1 className="text-white text-2xl font-bold">WebDetailed</h1>
         <nav>
           <ul className="flex space-x-4">
-            <li><a href="#" className="text-white hover:text-gray-300">Home</a></li>
-            <li><a href="#" className="text-white hover:text-gray-300">About</a></li>
-            <li><a href="#" className="text-white hover:text-gray-300">Services</a></li>
-            <li><a href="#" className="text-white hover:text-gray-300">Contact</a></li>
+            <li>
+              <Link href="/nextjs-15">Courses</Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.youtube.com/@WebDetailed"
+                target="_blank"
+                className="text-white hover:text-gray-300"
+              >
+                Youtube
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
