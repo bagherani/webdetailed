f1eaa7a2a8edaedf6f2bc90d0c5771cf70af44e6
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-11-09 02:02:44 +0100

    feat: more links added for nextjs course

--- src/app/(courses)/nextjs-15/page.tsx
+++ src/app/(courses)/nextjs-15/page.tsx
@@ -10,13 +10,19 @@ export default function NextJS() {
           <Link href="/nextjs-15/intro">Intro to Next.js</Link>
         </li>
         <li>
-          <Link href="/nextjs-15/create-next-app">Create Next.js App</Link>
+          <Link href="/nextjs-15/create-next-app">
+            Create Next.js Application
+          </Link>
         </li>
         <li>
-          <Link href="/nextjs-15/project-structure">Project Structure</Link>
+          <Link href="/nextjs-15/project-structure">
+            Next.js 15 Project Structure
+          </Link>
         </li>
         <li>
-          <Link href="/nextjs-15/configure-debugging">Configure Debugging</Link>
+          <Link href="/nextjs-15/configure-debugging">
+            Configure Debugging Next.js 15 App in VS Code
+          </Link>
         </li>
         <li>
           <Link href="/nextjs-15/routing-in-nextjs">
@@ -27,8 +33,51 @@ export default function NextJS() {
           <Link href="/nextjs-15/layouts-in-nextjs">Layouts in Next.js</Link>
         </li>
         <li>
+          <Link href="/nextjs-15/templates-in-nextjs">
+            Templates in Next.js 15 and their difference with Layouts
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/link-component">
+            Link Component in Next.js 15 and its Features
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/userouter-hook">
+            How to use the useRouter hook in Next.js 15
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/browser-pushstate">
+            Work with the Browser history and pushState function in Next.js
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/handle-errors">
+            How to Handle Unexpected Errors in Next.js 15
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/show-loading">
+            Show Loading at the Route Segment in Next.js 15
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/streaming-with-suspense">
+            Streaming with Suspense in Next.js 15
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/nextjs-redirections">
+            A Complete Guide to All 5 Redirection Methods with Examples
+          </Link>
+        </li>
+        <li>
+          <Link href="/nextjs-15/route-groups">Route Groups in Next.js</Link>
+        </li>
+        <li>
           <Link href="/nextjs-15/project-organization">
-            Project Organization
+            Optimal Folder Structure in Next.js
           </Link>
         </li>
       </ol>
