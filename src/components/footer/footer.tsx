88ea2e1fbe3d72108ced6b4d2b7c7fdf7ee9558f
Mohi Bagherani <mohammad.bagherani@gmail.com>
2024-12-03 16:22:06 +0100

    feat: getting the year updated for the footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,11 @@
+"use server";
+
 export const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="bg-gray-700 p-4">
       <div className="container mx-auto text-center">
-        <p className="text-white">© 2024 WebDetailed</p>
+        <p className="text-white">© {year} WebDetailed</p>
       </div>
     </footer>
   );
