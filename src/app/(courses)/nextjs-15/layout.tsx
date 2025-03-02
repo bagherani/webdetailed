122bb10a08264ef382512ee570bffec49f407c2f
Mohi Bagherani <mohammad.bagherani@gmail.com>
2025-03-02 18:09:26 +0100

    fix: projectid from commentbox added to the env variables

--- src/app/(courses)/nextjs-15/layout.tsx
+++ src/app/(courses)/nextjs-15/layout.tsx
@@ -1,11 +1,11 @@
-// import { CommentBox } from "@/components/comment-box/CommentBox";
+import { CommentBox } from "@/components/comment-box/CommentBox";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="prose px-4 mx-auto dark:prose-invert prose-lg">
       {children}
-      {/* {process.env.NODE_ENV === "production" && <CommentBox />} */}
+      {process.env.NODE_ENV === "production" && <CommentBox />}
     </div>
   );
 }
